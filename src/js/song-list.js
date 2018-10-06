b830e1014290a5a6bccd7fd1003c3716abb50789
Bunkang.Lee <liwenhansj@163.com>
2018-10-06 20:45:46 +0800

    获取歌曲列表 点击激活歌曲

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -26,6 +26,12 @@
 
 
         },
+        activeItem(li) {
+            let $li = $(li)
+            $li.addClass('active')
+                .siblings('.active')
+                .removeClass('active')
+        },
         clearActive() {
             $(this.el).find('.active').removeClass('active')
         }
@@ -33,13 +39,41 @@
     let model = {
         data: {
             songs: []
+        },
+        find() {
+            var query = new AV.Query('Song')
+            return query.find().then((songs) => {
+                this.data.songs = songs.map((song) => {
+                    return { id: song.id, ...song.attributes }
+                })
+                return songs
+            })
         }
     }
     let controller = {
         init() {
             this.view = view
             this.model = model
+
             this.view.render(this.model.data)
+            this.bindEvents()
+            this.bindEventsHub()
+            this.getAllSongs()
+
+
+        },
+        getAllSongs() {
+            return this.model.find().then(() => {
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents() {
+            $(this.view.el).on('click', 'li', (e) => {
+                this.view.activeItem(e.currentTarget)
+            })
+
+        },
+        bindEventsHub() {
             window.eventHub.on('upload', () => {
                 this.view.clearActive()
             })
