e78ba794fea2c57538717f3235c31ef19c5e70c7
Bunkang.Lee <liwenhansj@163.com>
2018-10-11 22:42:46 +0800

    播放和暂停

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,5 +1,29 @@
 {
-    let view = {}
+    let view = {
+        el: '#app',
+        template: `
+         <audio src={{url}}>
+
+         </audio>
+         <div>
+            <button class="play">播放</button>
+            <button class="pause">暂停</button>
+         </div>
+        `,
+        render(data) {
+
+            $(this.el).html(this.template.replace('{{url}}', data.url))
+            console.log(5555)
+        },
+        play() {
+            let audio = $(this.el).find('audio')[0]
+            audio.play()
+        },
+        pause() {
+            let audio = $(this.el).find('audio')[0]
+            audio.pause()
+        }
+    }
     let model = {
         data: {
             id: '',
@@ -15,6 +39,7 @@
 
             return query.get(id).then((song) => {
                 Object.assign(this.data, { id: song.id, ...song.attributes })
+                console.log(song)
                 return song
             })
         }
@@ -25,12 +50,22 @@
             this.model = model
             let id = this.getSongId()
 
-            this.model.setId(id)
+            // this.model.setId(id)
             this.model.get(id).then(() => {
                 console.log(this.model.data)
-
+                this.view.render(this.model.data)
+                this.bindEvents()
             })
         },
+        bindEvents() {
+            $(this.view.el).on('click', '.play', () => {
+                this.view.play()
+            })
+            $(this.view.el).on('click', '.pause', () => {
+                this.view.pause()
+            })
+        },
+
         getSongId() {
             let search = window.location.search
 
