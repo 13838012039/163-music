36cdf16ee9541c9da72839a7f56885c7c7712312
Bunkang.Lee <liwenhansj@163.com>
2018-10-11 18:24:58 +0800

    播放页面获取歌曲信息

--- src/js/index/page-1-2.js
+++ src/js/index/page-1-2.js
@@ -1,32 +1,36 @@
 {
     let view = {
         el: 'section.songs',
+        template: `
+        <li>
+        <h3>{{song.name}}</h3>
+        <p>
+            <svg class="icon icon-sq">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-sq" xlink:href>
+            </svg>> {{song.singer}}
+        </p>
+        <a class="playButton" href="./song.html?id={{song.id}}">
+            <svg class="icon icon-play">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play" xlink:href>
+            </svg>
+        </a>
+    </li>
+        `,
         init() {
             this.$el = $(this.el)
         },
         render(data) {
             console.log(3)
             let { songs } = data
-            console.log(songs)
+
             songs.map((song) => {
-                let $li = $(`
-                <li>
-                <h3>${song.name}</h3>
-                <p>
-                    <svg class="icon icon-sq">
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-sq" xlink:href>
-                    </svg>> ${song.singer}
-                </p>
-                <a class="playButton" href="#">
-                    <svg class="icon icon-play">
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play" xlink:href>
-                    </svg>
-                </a>
-            </li>
 
+                let $li = $(this.template
+                    .replace('{{song.name}}', song.name)
+                    .replace('{{song.singer}}', song.singer)
+                    .replace('{{song.id}}', song.id)
+                )
 
-
-                `)
                 this.$el.find('ol.list').append($li)
 
             })
